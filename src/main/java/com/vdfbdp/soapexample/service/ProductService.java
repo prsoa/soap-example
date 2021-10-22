2f44f8c48c8530a2df84eaf912f6c049f7b01433
Paulo Azevedo <paulo.azevedo@edirectinsure.com>
2021-10-22 12:14:33 +0100

    Initial project setup

---
+++ src/main/java/com/vdfbdp/soapexample/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.vdfbdp.soapexample.service;
+
+import java.util.HashMap;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class ProductService {
+
+    // private static final Map<String, Product> products = HashMap<>();
+
+    // @PostConstruct
+    // public void initialize() {
+
+    //     Product apple = new Product();
+
+
+    // }
+}
---
+++ src/test/java/com/vdfbdp/soapexample/SoapExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdfbdp.soapexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
